46ef666f5aa269def3118269d2f6b3e5fcd6d27d
Ailuridaek3k <a0z.universe@gmail.com>
2019-04-06 11:52:32 -0400

    shrey is the worst, added log in and register, to do index and gorup view

--- BookApp/index.php
+++ BookApp/index.php
@@ -49,117 +49,126 @@
 //$conference = $_SESSION['conference'];
 
 ?>
-    <!DOCTYPE html>
-    <html>
+<!DOCTYPE html>
+<html>
 
-    <head>
-        <title>bOOK aPP</title>
-        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <!--Import materialize.css-->
-        <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen,projection" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" /> </head>
-            <link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
-    <body class="grey lighten-4">
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-        <script type="text/javascript" src="js/materialize.min.js"></script>
-      
-  <nav>
-    <div class="nav-wrapper blue lighten-2">
-      <a href="index.php" class="brand-logo center">Books</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
+<head>
+    <title>bOOK aPP</title>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.css" media="screen,projection" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Source+Sans+Pro" rel="stylesheet">
+</head>
+<link href="https://fonts.googleapis.com/css?family=Muli" rel="stylesheet">
+
+<body class="grey lighten-4">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
+
+    <nav>
+        <div class="nav-wrapper blue lighten-2">
+            <a href="index.php" class="brand-logo center">Books</a>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                <li><a href="logout.php">Log out</a></li>
+            </ul>
+            <ul id="nav-mobile" class="right hide-on-med-and-down">
                 <li><a href="startGroup.php">Start your own book group</a></li>
+            </ul>
+        </div>
+    </nav>
 
-      </ul>
-    </div>
-  </nav>
-        
-             
-     <div class = "container">    <div class="card-panel" style="padding:20px"> <h1>My Groups</h1>
+
+    <div class="container">
+        <div class="card-panel" style="padding:20px">
+            <h1>My Groups</h1>
             <?php
 			if(!count($records2)) {
 
 				echo 'no teams found'; 
 				} else {  
 		?>
-         
-      
-                <table style = "" class="responsive-table striped">
-                    <thead>
-                        <tr>
-                            <th>Title</th>
-                            <th>Author</th>
-                            <th>Genre</th>
-                            <th></th>
-                            
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <?php
+
+
+            <table style="" class="responsive-table striped">
+                <thead>
+                    <tr>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Genre</th>
+                        <th></th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php
 				foreach($records2 as $r){
 				?>
-                            <tr>
-                                <td>
-                                    <?php echo $r->Title; ?>
-                                </td>
-                                <td>
-                                    <?php echo $r->Author; ?>
-                                </td>
-                                <td>
-                                    <?php echo $r->Genre; ?>
-                                </td>
-                                <td>
-                                    <?php echo "<td><a href='enterBookGroup.php?id=".$r->Title."'>Enter</a></td>"?>
-                                </td>
-                               
-                            </tr>
-                            <?php
+                    <tr>
+                        <td>
+                            <?php echo $r->Title; ?>
+                        </td>
+                        <td>
+                            <?php echo $r->Author; ?>
+                        </td>
+                        <td>
+                            <?php echo $r->Genre; ?>
+                        </td>
+                        <td>
+                            <?php echo "<td><a href='enterBookGroup.php?id=".$r->Title."'>Enter</a></td>"?>
+                        </td>
+
+                    </tr>
+                    <?php
 			}
 				?>
-                    </tbody>
-                </table>
-                <?php 
+                </tbody>
+            </table>
+            <?php 
 					} 
 				?>
-         </div> 
-  <div class = "container">    <div class="card-panel" style="padding:20px"> <h1>All Groups</h1>
-            <?php
+        </div>
+        <div class="container">
+            <div class="card-panel" style="padding:20px">
+                <h1>All Groups</h1>
+                <?php
 			if(!count($records)) {
 
 				echo 'no teams found'; 
 				} else {  
 		?>
-         
-      
-                <table style = "" class="responsive-table striped">
+
+
+                <table style="" class="responsive-table striped">
                     <thead>
                         <tr>
                             <th>Title</th>
                             <th>Author</th>
                             <th>Genre</th>
                             <th></th>
-                            
+
                         </tr>
                     </thead>
                     <tbody>
                         <?php
 				foreach($records as $r){
 				?>
-                            <tr>
-                                <td>
-                                    <?php echo $r->Title; ?>
-                                </td>
-                                <td>
-                                    <?php echo $r->Author; ?>
-                                </td>
-                                <td>
-                                    <?php echo $r->Genre; ?>
-                                </td>
-                                <td>
-                                    <?php echo "<td><a href='joinBookGroup.php?id=".$r->Title."'>Join Group</a></td>"?>
-                                </td>
-                               
-                            </tr>
-                            <?php
+                        <tr>
+                            <td>
+                                <?php echo $r->Title; ?>
+                            </td>
+                            <td>
+                                <?php echo $r->Author; ?>
+                            </td>
+                            <td>
+                                <?php echo $r->Genre; ?>
+                            </td>
+                            <td>
+                                <?php echo "<td><a href='joinBookGroup.php?id=".$r->Title."'>Join Group</a></td>"?>
+                            </td>
+
+                        </tr>
+                        <?php
 			}
 				?>
                     </tbody>
@@ -167,9 +176,9 @@
                 <?php 
 					} 
 				?>
-         </div> 
+            </div>
 
 
-    </body>
+</body>
 
-    </html>
+</html>
